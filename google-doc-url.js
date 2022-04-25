2637114eff64d33c82bfc9b137dc267489fed7e9
waszka-paszka <103440096+waszka-paszka@users.noreply.github.com>
2022-04-25 16:27:21 +0200

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -2,4 +2,4 @@
 var googleDocURL = 'https://docs.google.com/spreadsheets/d/16oHL-plvv9qhUEbCavH5xDvpaUQGCZOJkf2t2I2W71U/edit?fbclid=IwAR0emVxZ_52afGpe57gWuhcwqMb6DwHpq4w8Heg5fEqLg3wkYpmjqNbPFzY#gid=0';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
-var googleApiKey = 'AIzaSyAtJarj0kmrj8KxD8kHrTjwhsEMiCTwJ94';
+var googleApiKey = 'AIzaSyBPA5fmrRS6pZwBSfSDC2a-12xbwnu15Q0';
